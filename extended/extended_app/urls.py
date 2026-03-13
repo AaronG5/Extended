efe10b284aed3930c9fe65792bb77a6fa4c73b53
Aaron Gandz <142338706+AaronG5@users.noreply.github.com>
2026-03-14 00:02:21 +0200

    Esp32 endpoint (#2)
    * Create database models
    
    * Change directory name
    
    * Add requirements.txt and app to settings
    
    * Add database file
    
    * Update database with migrations
    
    * Add button_state field
    
    * Create serializer and view for esp32 endpoint data and update migrations
    
    * Update views to match models
    
    * Migration change
    
    * Create generated test and fixes based on tests
    
    * Create docker compose
    
    * Implement database mirroring for docker container
    
    * Create view for dashboard
    
    * Remove comment

--- extended/extended_app/urls.py
+++ extended/extended_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ReceiveReadingsView
+from .views import ReceiveReadingsView, ESP32DashboardView
 
 urlpatterns = [
     path('readings/', ReceiveReadingsView.as_view(), name='receive-readings'),
+    path('dashboard/<str:esp32_id>/', ESP32DashboardView.as_view(), name='esp32-dashboard'),
 ]

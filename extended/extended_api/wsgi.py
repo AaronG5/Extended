ae6ca12b18ae73c33b7284ad6f4e962510fdff49
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-03-14 14:25:37 +0200

    last

--- extended/extended_api/wsgi.py
+++ extended/extended_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'extended.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'extended_api.settings')
 
 application = get_wsgi_application()
